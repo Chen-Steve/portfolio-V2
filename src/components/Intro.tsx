ac239c6d91719229738de136306032a25511b4ed
Steven <zhangc2@illinois.edu>
2023-10-20 01:02:21 -0500

    basic changes

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import TypeWriter from 'react-typewriter';
 import styles from "@/styles/Intro.module.css";
 
 const Intro: React.FC = () => {
@@ -7,22 +5,20 @@ const Intro: React.FC = () => {
     <div id="intro" className={styles.intro}>
       <div className={styles["cmd-style"]}>
         <div className={styles["intro-name"]}>{`SC:-/$\u00A0`}</div>
-        <span className={styles["intro-title"]}>
-          {"hello, "}
-          <TypeWriter typing={1}>
-            <span className={styles["intro-name"]}>Steven</span>
-          </TypeWriter>
-          {" here"}
-        </span>
+          <span className={styles["intro-title"]}>
+            {"hello, "}
+            <span className={styles["intro-name"]}>{"Steven"}</span>
+            {" here"}
+          </span>
       </div>
-      <div className={styles["intro-subtitle"]}>
-        Good things take time... finishing soon!
-      </div>
-      <div className={styles["intro-desc"]}>
-        I&apos;m an aspiring software engineer with a passion for full-stack
-        development, artificial intelligence, and machine learning. Let&apos;s
-        build something amazing together!
-      </div>
+        <div className={styles["intro-subtitle"]}>
+          Good things take time... finishing soon!
+        </div>
+        <div className={styles["intro-desc"]}>
+          I&apos;m a aspiring software engineer with a passion for full-stack
+          development, artificial intelligence, and machine learning. Let&apos;s
+          build something amazing together!
+        </div>
     </div>
   );
 }
