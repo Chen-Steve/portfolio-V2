e0b63731d3ba112dbec2997c80d6de73985bd6cb
Steven <zhangc2@illinois.edu>
2023-10-24 00:13:11 -0500

    added skeleton version of Projects carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,37 +1,55 @@
 import React from "react";
-
 import Image from "next/image";
-
-import useEmblaCarousel from 'embla-carousel-react'
 import styles from "@/styles/Projects.module.css";
-import "@/styles/Carousel.css"
+import "@/styles/Carousel.css";
 import Carousel from "react-bootstrap/Carousel";
-
 import SocialLinks from "@/components/SocialLinks";
 import "bootstrap/dist/css/bootstrap.css";
 
+const Projects = () => {
+  const projects = [
+    { title: 'Secure Surf', description: 'Description 1', imageUrl: '/public/images/website.png' },
+    { title: 'GeoHarbin', description: 'Description 2', imageUrl: '/public/images/website.png' },
+    { title: 'My Website!', description: 'Description 2', imageUrl: '/public/images/website.png' }
+    // more projects in the future
+  ];
 
-const PROJECTS = [
-    { title: "Project 1", description: "Description" },
-    { title: "Project 2", description: "Description" },
-    { title: "Project 3", description: "Description" },
-];
-
-const Projects: React.FC = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
-
-    return (
-        <div ref={emblaRef} className={styles.carousel}>
-            <div className={styles.carousel__viewport}>
-                {PROJECTS.map((project) => (
-                    <div key={project.title} className={styles.carousel__slide}>
-                        <h3 className={styles.projectTitle}>{project.title}</h3>
-                        <p className={styles.projectDescription}>{project.description}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div id="projects" className={styles.projects}>
+      <div className={styles["section-header"]}>
+        <span className={styles["section-title"]}>/ My Projects</span>
+      </div>
+      <div className={styles["spotlight-container"]}>
+        <Carousel>
+          {projects.map((project, index) => (
+            <Carousel.Item key={index}>
+              <Image
+                src={project.imageUrl}
+                alt={project.title}
+                placeholder="empty"
+                className="d-block w-100"
+                sizes="100vw"
+                width={0}
+                height={0}
+                style={{
+                  objectFit: "contain",
+                  width: "100%",
+                  height: "auto",
+                  opacity: "0.5",
+                }}
+              />
+              <div className={styles["caption-bg"]}>
+                <Carousel.Caption>
+                  <h3>{project.title}</h3>
+                  <div>{project.description}</div>
+                </Carousel.Caption>
+              </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
