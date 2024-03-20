6f07bc99fac6cc000fffb73e6a7c26b99da79740
Steven <zhangc2@illinois.edu>
2024-03-20 13:05:33 -0500

    added more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,11 +7,13 @@ import "bootstrap/dist/css/bootstrap.css";
 
 const Projects = () => {
   const projects = [
-    { title: 'CPU Monitor Electron DeskApp', description: 'A cross-platform desktop application built with Electron.js.', imageUrl: '/images/cpuMonitor.png', url: 'https://drive.google.com/drive/folders/1CdnR0ll3LlbUhr-AncfTZts2DauDL7S6?usp=sharing'},
+    { title: 'Particle Physics Simulator', description: 'A cool little particle simulator with full physics control', imageUrl: '/images/Particle.png', url: 'https://github.com/Chen-Steve/particle-life-main'},
     { title: 'Servicemen to Scholars', description: 'A non-profit MENN stack website + gpt-4 for tailoring personal scholarships & grants', imageUrl: '/images/s2s.png', url: 'https://servicemen-to-scholars.vercel.app/'},
+    { title: 'SpoonFul', description: 'Spoonful is a service and mobile app designed to bridge the gap between households and food kitchens', imageUrl: '/images/SpoonFul.png', url: 'https://github.com/Chen-Steve/SpoonFulWeb'},
+    { title: 'BlackJack', description: 'A cool BlackJack game made with simple html css and js', imageUrl: '/images/BlackJack.png', url: 'https://github.com/Chen-Steve/Poker'},
     { title: 'Secure Surf', description: 'Web app + Twilio text bot for testing domain safety. Trained with gpt-3.5-turbo', imageUrl: '/images/secure_surf.png', url: 'https://github.com/shashankdatta/secure-surf'},
+    { title: 'CPU Monitor Electron DeskApp', description: 'A cross-platform desktop application built with Electron.js.', imageUrl: '/images/cpuMonitor.png', url: 'https://drive.google.com/drive/folders/1CdnR0ll3LlbUhr-AncfTZts2DauDL7S6?usp=sharing'},
     { title: 'My personal Website!', description: 'Built with Next.js, TypeScript and React.js', imageUrl: '/images/website.png' },
-    { title: 'SpoonFul', description: 'A fullstack website to reduce household waste', imageUrl: '/images/SpoonFul.png', url: 'https://github.com/Chen-Steve/SpoonFulWeb'},
   ];
   return (
     <div id="projects" className={styles.projects}>
